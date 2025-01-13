e584ad2ffb2cb297062d43242a7f019a26d00214
drunksweet <2141809608@qq.com>
2025-01-13 16:08:45 +0800

    styles:调整了一下groupPage的布局

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import routes from "./routes";
 import "./styles/base.scss";
 
 function App() {
+  //todo: 小神秘啊，居然写了移动端都没人说，后端完全没部署，找个人修一修
   // 检测是否为移动设备
-  if (/Mobi|Android/i.test(navigator.userAgent)) {
-    // 执行移动设备重定向
-    window.location.href = "http://m.muxi-tech.xyz";
-  }
+  // if (/Mobi|Android/i.test(navigator.userAgent)) {
+  //   // 执行移动设备重定向
+  //   window.location.href = "http://m.muxi-tech.xyz";
+  // }
   const element = useRoutes(routes);
   return (
     <div className="App">
