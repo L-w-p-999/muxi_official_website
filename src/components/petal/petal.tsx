5f6dcccc24922d46a4b924e9b2c050081958e73a
drunksweet <2141809608@qq.com>
2025-01-13 19:54:09 +0800

    fix:修复落花元素溢出bug feature:随机落花效果

--- src/components/petal/petal.tsx
+++ src/components/petal/petal.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import './petal.scss';
 
-const Petal:React.FC = ()=> {
-    return(
-            
-        <div className = "fallingLeaves">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+const Petal: React.FC = () => {
+    return (
 
-        <span style = {{position:'relative',left:'80%'}}></span>
-        <span style = {{position:'relative',left:'35%',top:'15%'}}></span>
-        <span  style = {{position:'relative',left:'25%',top:'5%'}}></span>
-        <span style = {{position:'relative',left:'40%',top:'15%'}}></span>
-        <span  style = {{position:'relative',left:'30%',top:'5%'}}></span>
-        <span  style = {{position:'relative',left:'45%',top:'10%'}}></span>
-        <span style = {{position:'relative',left:'65%',top:'10%'}}></span>
-        <span style = {{position:'relative',left:'55%',top:'15%'}}></span>
+        <div className="fallingLeaves">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
-       
 
-)
+
+    )
 }
 export default Petal;
